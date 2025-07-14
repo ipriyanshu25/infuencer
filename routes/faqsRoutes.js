0cf10947bd65bf03f5eee52947f99a960a7d33d5
Priyanshu Yadav <114854534+ipriyanshu25@users.noreply.github.com>
2025-07-14 13:10:35 +0530

    update

--- routes/faqsRoutes.js
+++ routes/faqsRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const faqCtrl = require('../controllers/faqsController');
 
 router.post('/create',    faqCtrl.createFAQ);
-router.get('/get',        faqCtrl.getAllFAQs);
+router.post('/get',        faqCtrl.getAllFAQs);
 router.post('/getById',   faqCtrl.getFAQById);
 router.post('/updateById',faqCtrl.updateFAQ);
 router.post('/deleteById',faqCtrl.deleteFAQ);

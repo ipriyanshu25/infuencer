2042bc4693b539f88079c49701182fc5f4afaf86
PriyanshuYadav <priyanshuyad2001@gmail.com>
2025-07-18 13:18:32 +0530

    update

--- app.js
+++ app.js
@@ -119,7 +119,9 @@ wss.on('connection', (ws, req) => {
 
 // CORS + body parsers
 app.use(cors({
+  // origin: process.env.FRONTEND_ORIGIN || 'https://collabglam.com',
   origin: process.env.FRONTEND_ORIGIN || 'http://localhost:3000',
+
   credentials: true
 }));
 app.use(express.json());

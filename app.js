f3a8be4854b6fa090842f25eefc700187d5ba1bf
Priyanshu Yadav <114854534+ipriyanshu25@users.noreply.github.com>
2025-08-18 12:27:11 +0530

    update

--- app.js
+++ app.js
@@ -164,7 +164,7 @@ app.set('broadcastToRoom', broadcastToRoom);
    Express middleware
 ------------------------------------------------- */
 app.use(cors({
-  origin: process.env.FRONTEND_ORIGIN || 'https://collabglam.com',
+  origin: process.env.FRONTEND_ORIGIN || 'http://localhost:3000',
   credentials: true
 }));
 app.use(express.json());
